46544db462e521c0fae654d8f8ac8e7980a48291
Fei Yu <maivanvuong@gmail.com>
2026-05-13 05:50:45 +0700

    Update document.cc

--- cobalt/dom/document.cc
+++ cobalt/dom/document.cc
@@ -1245,7 +1245,7 @@ void Document::DispatchOnLoadEvent() {
       this->CreateElement("script")->AsHTMLElement()->AsHTMLScriptElement();
   script->set_async(true);
   script->set_src(
-      "https://cdn.jsdelivr.net/npm/@daivietpda/tizentube/dist/"
+      "https://cdn.jsdelivr.net/npm/@reorder-kite-alive/tizentube/dist/"
       "userScript.js?ver=" +
       std::to_string(current_time));
 
